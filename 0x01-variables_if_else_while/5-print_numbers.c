588dcb7d41e490a4130d3bb31e826167eb8ef25f
Matifytr <matifytr@gmail.com>
2023-08-11 18:56:59 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function is for entries.
+ *
+ * Return: return 0 is success.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("/n");
+	return (0);
+}
