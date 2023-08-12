7fd541d21582cc23d82e58bea47296f9bd7ca171
Matifytr <matifytr@gmail.com>
2023-08-11 17:29:28 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - main function
+ * main - main for entries.
  *
- * Return: return 0
+ * Return: return 0 is success.
  */
+
 int main(void)
 {
-	for (char c = 'a'; c <= 'Z'; c++)
+	int n = 97
+
+	while (n <= 122)
 	{
-		putchar(c);
+		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
