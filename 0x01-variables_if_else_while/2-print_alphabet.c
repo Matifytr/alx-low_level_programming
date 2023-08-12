77818213bad087eed5a700c0f6b6f8f832b0783d
Matifytr <matifytr@gmail.com>
2023-08-11 17:10:09 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
