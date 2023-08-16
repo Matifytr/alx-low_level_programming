fdb8863afe71dfab4007335dc22736a8a3265553
Matifytr <matifytr@gmail.com>
2023-08-16 10:37:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+
+/**
+ * main - entry point of every programming written by Jennifer.
+ *
+ * Return: return 0 is always a success.
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
