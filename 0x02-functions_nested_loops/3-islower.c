c7856980391bb73021d39e64be32b64b9b4a55d1
Matifytr <matifytr@gmail.com>
2023-08-16 12:08:38 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check the char case.
+ * Return: 1 if char case is confirmed, otherwise 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <== 'z')
+		return (1);
+	else
+		return (0);
+}
