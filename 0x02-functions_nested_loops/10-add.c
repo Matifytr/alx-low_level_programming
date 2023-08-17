37514b5e915f22d2ed0c6203fb1f5993a70a349f
Matifytr <matifytr@gmail.com>
2023-08-16 18:38:59 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n3: The first number computed
+ * @n4: The second number computed
+ * Return: Integer value
+ */
+
+int add(int n3, int n4)
+{
+return (n3 + n4);
+}
